213eefe158c301144cfa329e3f8f694fca31565d
Terry Ouyang <yamerocat520@gmail.com>
2020-04-07 22:50:49 +0800

    note

--- bk.py
+++ bk.py
@@ -1,27 +1,28 @@
+# 讀取檔案
 products = []
-
-
 with open('products.csv', 'r', encoding='utf-8') as f:
 	for line in f:
 		if '商品, 價格' in line:
 			continue
 		name, price = line.strip().split(',')
 		products.append([name, price])
-
 print(products)
 
-
+# 輸入商品
 while True:
 	name = input('商品名稱: ')
 	if name == 'q':
 		break
 	price = input('商品價格: ')
 	products.append([name, price])
+
+# 印出紀錄
 for p in products:
 	print(p[0], '的價格是', p[1], '元')
 
 # products[0][0] 第一大框裡的第一小框
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品, 價格 \n')
 	for p in products:
