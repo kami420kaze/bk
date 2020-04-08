ab8c10c3741dafffdbedc0b2df9d46387eeacb76
Terry Ouyang <yamerocat520@gmail.com>
2020-04-08 11:48:42 +0800

    check file

--- bk.py
+++ bk.py
@@ -1,12 +1,19 @@
-# 讀取檔案
+# 檢查+讀取檔案
+import os # operating system
+
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品, 價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(products)
+
+if os.path.isfile('products.csv'):
+	print('fuck yea boi')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品, 價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+		print(products)
+else:
+	print('nah man')
 
 # 輸入商品
 while True:
